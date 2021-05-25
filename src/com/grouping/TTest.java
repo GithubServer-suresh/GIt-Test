01f9b2b3f3129cbecc17ade16c9f05c3a1f75071
GithubServer-suresh <ummitisuresh123@gmail.com>
2021-05-25 16:46:46 +0530

    Update TTest.java

--- src/com/grouping/TTest.java
+++ src/com/grouping/TTest.java
@@ -3,10 +3,7 @@ package com.grouping;
 public class TTest {
 
 	public static void main(String[] args) {
-		//THis is for GIt merge conflict
-
-		//This is for head check
-		//Optional demo check
+		//This is for merge conflict
 
 		
 	}
